0e04bc9876ca3e8f3286fa7d1684935705790a8e
hjchen2 <chenhoujiangcug@gmail.com>
2024-10-23 07:05:27 +0000

    update

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -2,4 +2,4 @@ set -Eeuo pipefail
 which python
 which pip
 
-pip install -U setuptools_scm ninja Cython "torch==${TORCH_VERSION}" -r requirements.txt --no-cache-dir
+pip install -U setuptools_scm ninja Cython "torch==${TORCH_VERSION}" -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cpu --no-cache-dir
